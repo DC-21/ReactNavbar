239c80a304f47a1667ac571dc113001097c858a6
DC-21 <cholahkuboko@gmail.com>
2023-05-15 16:33:05 +0200

    commit message

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <header className='text-gray-600 body-font'>
+      <nav className="container mx-auto px-2 md:px-4 lg:px-8 w-full flex">
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0' href="https://vitejs.dev" target="_blank"><span class="ml-5 text-xl">D<span class="text-red-900">i</span>CE</span></a>
         <nav className='md:ml-auto md: mr-auto flex flex-wrap items-center text-base justify-center'>
@@ -14,7 +15,7 @@ const Navbar = () => {
         <button className='inline-flex items-center bg-blue-900 border-0 py-1 px-3 focus:outline-none hover:by-gray-900 rounded text-base mt-4 md:mt-0'>Get started</button>
         <button></button>
       </div>
-      <div></div>
+      </nav>
     </header>
   )
 }
