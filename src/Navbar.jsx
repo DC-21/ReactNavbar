ee03d70848b6438b257729306bcf82efa5ab1aed
DC-21 <cholahkuboko@gmail.com>
2023-05-16 16:06:16 +0200

    commit message

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className='text-gray-600 body-font'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-        <div className="divide-gray-700 mt-2 text-2xl text-indigo-800 cursor-pointer"><ion-icon name="logo-xbox"></ion-icon></div>
+        <div className="divide-gray-700 mt-2 text-3xl text-indigo-800 cursor-pointer"><ion-icon name="dice-outline"></ion-icon></div>
         <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0' href="https://vitejs.dev" target="_blank"><span class="ml-5 text-xl">D<span class="text-red-900">i</span>CE</span></a>
         <nav className='md:ml-auto md: mr-auto flex flex-wrap items-center text-base justify-center'>
             <a className='mr-5 hover:text-blue-900' href="#">Home</a>
