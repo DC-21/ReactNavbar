b78d7c3957510edfa35f46f1e56952d5080d379c
DC-21 <cholahkuboko@gmail.com>
2023-05-16 16:32:17 +0200

    commit message

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <div className="divide-gray-700 mt-2 text-3xl text-indigo-800 cursor-pointer font-bold"><ion-icon name="dice-outline"></ion-icon></div>
         <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0' href="https://vitejs.dev" target="_blank"><span class="ml-5 text-xl font-bold">D<span class="text-red-900 font-bold">i</span>CE</span></a>
-        <nav className='md:ml-auto md: mr-auto flex flex-wrap items-center text-xl justify-center'>
+        <nav className='md:ml-auto md:mr-auto flex flex-row items-center text-xl justify-center'>
             <a className='mr-5 hover:text-blue-900' href="#">Home</a>
             <a className='mr-5 hover:text-blue-900' href="#">Services</a>   
             <a className='mr-5 hover:text-blue-900' href="#">Contact</a>
